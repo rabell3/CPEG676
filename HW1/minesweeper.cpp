a5ec14d89ceb4ae6636e112d21c0058f89c0fd0c
robert bell <rabell3@gmail.com>
2018-04-17 21:09:04 -0400

    Generate gameboard structure. User input section in place.

--- HW1/minesweeper.cpp
+++ HW1/minesweeper.cpp
@@ -3,19 +3,21 @@
 #include <string>
 using namespace std;
 
-enum DIFFICULTY { Easy, Medium, Difficult };
+enum DIFFICULTY { Easy, Medium, Hard };
 
 class MinesweepGameboard
 {
     public:
         MinesweepGameboard();
-        void createGameboard(int x, int y, int mines, DIFFICULTY level);
-        void getDimensions(int x, int y);
+        void createGameboard(int x, int y, int mines, DIFFICULTY level);  // should meet requirement 1
+        int getXdim(int x) { return dim_x; }
+        int getydim(int y) { return dim_y; }
+        void printGameboard();
         ~MinesweepGameboard();
 
     private:
-        void setDimensions(int x, int y);
-        void setMines(int mines);
+//        void setDimensions(int x, int y);
+//        void setMines(int mines);
         DIFFICULTY difficulty;
         int dim_x;
         int dim_y;
@@ -38,6 +40,7 @@ MinesweepGameboard::~MinesweepGameboard()
     cout << "Minesweeper object deleted.\n";
 }
 
+/*
 void MinesweepGameboard::setDimensions(int x, int y)
 {
     cout << "Setting dimensions of gameboard...\n";
@@ -49,19 +52,44 @@ void MinesweepGameboard::setMines(int mines)
 {
     cMines = mines;
 }
+*/
 
-void MinesweepGameboard::createGameboard(int x, int y, int mines, DIFFICULTY level)
+void MinesweepGameboard::createGameboard(int width, int height, int mines, DIFFICULTY level)
 {
     cout << "Gameboard created.\n";
-    dim_x = x;
-    dim_y = y;
+    dim_x = width;
+    dim_y = height;
     cMines = mines;
     difficulty = level;
 }
 
 main(){
+    char level;
+
     printf("HW1: CLI Minesweeper - RABIII\n");
     MinesweepGameboard msGame;
-    msGame.createGameboard(10,20,10,Easy);
+
+    printf("Choose level of difficulty (E)asy, (M)edium, (H)ard): ");
+    cin >> level;
+    switch (level)
+    {
+        case 'E':
+            cout << "Generating an Easy gameboard...\n";
+            msGame.createGameboard(10,6,10,Easy);
+            break;
+        case 'M':
+            cout << "Generating a Medium gameboard...\n";
+            msGame.createGameboard(20,12,20,Medium);
+            break;
+        case 'H':
+            cout << "Generating a Difficult gameboard...\n";
+            msGame.createGameboard(30,18,30,Hard);
+            break;
+        default:
+            cout << "This shouldn't happen\n";
+            break;
+    }
+
+        
     return 0;
 }
