6076eb788a4847849f4efc2ef7783d18d8c55fc5
robert bell <rabell3@gmail.com>
2018-04-18 20:19:22 -0400

    Updated setmines.

--- HW1/minesweeper.cpp
+++ HW1/minesweeper.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string>
+#include <time.h>
 using namespace std;
 
 enum DIFFICULTY { Easy, Medium, Hard };
@@ -9,8 +11,8 @@ class MinesweepGameboard
 {
     public:
         MinesweepGameboard();
-        MinesweepGameboard(int x, int y, int mines, DIFFICULTY level);
-        void createGameboard(int x, int y, int mines, DIFFICULTY level);  // should meet requirement 1
+        MinesweepGameboard(int x, int y, DIFFICULTY level);
+        void createGameboard(int x, int y, DIFFICULTY level);  // should meet requirement 1
         int getXdim(int x) { return dim_x; }
         int getydim(int y) { return dim_y; }
         void printGameboard();
@@ -19,7 +21,7 @@ class MinesweepGameboard
 
     private:
 //        void setDimensions(int x, int y);
-//        void setMines(int mines);
+        int setMines(DIFFICULTY difficulty);
         DIFFICULTY difficulty;
         int dim_x;
         int dim_y;
@@ -38,9 +40,9 @@ MinesweepGameboard::MinesweepGameboard()
 }
 
 // Constructor
-MinesweepGameboard::MinesweepGameboard(int x, int y, int mines, DIFFICULTY level)
+MinesweepGameboard::MinesweepGameboard(int x, int y, DIFFICULTY level)
 {
-    createGameboard(x, y, mines, level);
+    createGameboard(x, y, level);
     cout << "Proper Minesweeper object initialized.\n";
 }
 
@@ -57,20 +59,41 @@ void MinesweepGameboard::setDimensions(int x, int y)
     dim_x = x;
     dim_y = y;
 }
-
-void MinesweepGameboard::setMines(int mines)
-{
-    cMines = mines;
-}
 */
+int MinesweepGameboard::setMines(DIFFICULTY difficulty)
+{
+    srand (time(NULL));
+    int easyMines = rand() % 10 + 1;
+    int mediumMines = rand() % 20 + 11;
+    int hardMines = rand() % 30 + 21;
 
-void MinesweepGameboard::createGameboard(int width, int height, int mines, DIFFICULTY level)
+    switch (difficulty)
+    {
+        case Easy:
+            return easyMines;
+            break;
+        case Medium:
+            return mediumMines;
+            break;
+        case Hard:
+            return hardMines;
+            break;
+        default:
+            // This shouldn't happen
+            return 0;
+            break;
+    }
+}
+
+void MinesweepGameboard::createGameboard(int width, int height, DIFFICULTY level)
 {
     cout << "Gameboard created.\n";
     dim_x = width;
     dim_y = height;
-    cMines = mines;
     difficulty = level;
+    cMines = setMines(difficulty);
+
+    
     gameArray[width,height];
 }
 void MinesweepGameboard::printGameboard()
@@ -114,7 +137,7 @@ main(){
             cout << "Generating an Easy gameboard...\n";
             thisX = 10;
             thisY = 6;
-            thisMines = 20;
+            thisMines = 10;
             thisDifficulty = Easy;
             break;
         case 'M':
@@ -135,7 +158,7 @@ main(){
             cout << "This shouldn't happen\n";
             break;
     }
-    MinesweepGameboard msGame(thisX, thisY, thisMines, thisDifficulty);
+    MinesweepGameboard msGame(thisX, thisY, thisDifficulty);
     msGame.printGameboard();
     return 0;
 }
