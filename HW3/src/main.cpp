a99bbd93309052568b1e7b40aafddea16049ce85
robert bell <rabell3@gmail.com>
2018-05-17 23:32:53 -0400

    little progress

--- HW3/src/main.cpp
+++ HW3/src/main.cpp
@@ -1,26 +1,46 @@
 #include <stdio.h>
+#include <string>
 #include "db.h"
 
-int main(int argc, char **argv){
-  sqlite3 *db;
+int main(){
+  char* dbname="geemail.db";
+  sqlite3 *db;  // database handle
   char *zErrMsg = 0;
   int rc;
 
-  if( argc!=3 ){
-    fprintf(stderr, "Usage: %s DATABASE SQL-STATEMENT\n", argv[0]);
+  char* db_query1="create table person (id integer primary key, name text, age int);";
+  char* db_query2="insert into person (name, age) values ('Robert', '39');";
+  char* db_query3="insert into person (name, age) values ('Mary', '40');";
+/*
+  if( argc!=2 ){
+    //fprintf(stderr, "Usage: %s DATABASE SQL-STATEMENT\n", argv[0]);
+    fprintf(stderr, "Usage: %s SQL-STATEMENT\n", argv[0]);
     return(1);
   }
-  rc = sqlite3_open(argv[1], &db);
+*/
+  rc = sqlite3_open(dbname, &db);
   if( rc ){
     fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
     sqlite3_close(db);
     return(1);
   }
-  rc = sqlite3_exec(db, argv[2], callback, 0, &zErrMsg);
+  //rc = sqlite3_exec(db, argv[2], callback, 0, &zErrMsg);
+  rc = sqlite3_exec(db, db_query1, callback, 0, &zErrMsg);
   if( rc!=SQLITE_OK ){
     fprintf(stderr, "SQL error: %s\n", zErrMsg);
     sqlite3_free(zErrMsg);
   }
+  rc = sqlite3_exec(db, db_query2, callback, 0, &zErrMsg);
+  if( rc!=SQLITE_OK ){
+    fprintf(stderr, "SQL error: %s\n", zErrMsg);
+    sqlite3_free(zErrMsg);
+  }
+  rc = sqlite3_exec(db, db_query3, callback, 0, &zErrMsg);
+  if( rc!=SQLITE_OK ){
+    fprintf(stderr, "SQL error: %s\n", zErrMsg);
+    sqlite3_free(zErrMsg);
+  }
+
   sqlite3_close(db);
   return 0;
 }
